8511d815a274b30e2345920729c09ab5f9ed45eb
Tom Mason <wheybags@wheybags.com>
2016-12-07 13:54:14 +0000

    update to add windows support

--- scripts/windows_pre_build.bat
+++ scripts/windows_pre_build.bat
@@ -13,20 +13,49 @@ if %REALARCH%==x64 (
 	)
 )
 
-set "DLL_SUFFIX="
-if %CONFIG%==Debug set "DLL_SUFFIX=d"
+set CURRDIR=%CD%
+
 
 echo "--------------------------" %PROJDIR% %CONFIG% %REALARCH%
 
-set "TOP_DIR=%PROJDIR%\..\..\..\.."
-set "BUILD_DIR=%TOP_DIR%\src_c\build_%CONFIG%_%REALARCH%"
-set "C_DLL=%BUILD_DIR%\inst\lib\AIL%DLL_SUFFIX%.dll"
+cd "%PROJDIR%"
+
+set "NATIVE_DIR="
+for /F "skip=1 delims=" %%i in (native_code_setting.txt) do if not defined NATIVE_DIR set "NATIVE_DIR=%%i"
+
+:: replace forward with backslashes
+set "NATIVE_DIR=%NATIVE_DIR:/=\%"
+:: make absolute
+set "NATIVE_DIR=%PROJDIR%%NATIVE_DIR%"
+
+set "LIBNAME="
+for /F "skip=2 delims=" %%i in (native_code_setting.txt) do if not defined LIBNAME set "LIBNAME=%%i"
+
+set "CMAKE_ARGS="
+for /F "skip=3 delims=" %%i in (native_code_setting.txt) do if not defined CMAKE_ARGS set "CMAKE_ARGS=%%i"
+
+
+cd %CURRDIR%
+
+if not exist "%NATIVE_DIR%" ( 
+    echo "#################################################"
+    echo "Your native source code directory doesn't exist!"
+    echo "Edit this file to change it: %PROJDIR%\native_code_setting.txt"
+    echo "Current evaluated setting: %NATIVE_DIR%"
+    echo "Current contents:"
+    type "%PROJDIR%\native_code_setting.txt"
+
+    exit 1
+)
+
+
+set "BUILD_DIR=%NATIVE_DIR%build_%CONFIG%_%REALARCH%"
+set "C_DLL=%BUILD_DIR%\inst\lib\%LIBNAME%.dll"
 
 echo %BUILD_DIR%
 
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-set CURRDIR=%CD%
 
 cd "%BUILD_DIR%"
 
@@ -34,20 +63,29 @@ set "binfile=native_code_windows_%REALARCH%"
 :: delete the old one first, just to be sure
 if exist "%PROJDIR%\embedded_files\%binfile%" del "%PROJDIR%\embedded_files\%binfile%"
 
-cmake .. -DCMAKE_BUILD_TYPE=%CONFIG% -DPYTHON_ENABLED=Off -DCMAKE_INSTALL_PREFIX=inst -G "Visual Studio 14%ARCH%"
-if %errorlevel% neq 0 exit %errorlevel%
+echo cmake .. %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=%CONFIG% -DCMAKE_INSTALL_PREFIX=inst -G "Visual Studio 14%ARCH%"
+cmake .. %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=%CONFIG% -DCMAKE_INSTALL_PREFIX=inst -G "Visual Studio 14%ARCH%"
+if %errorlevel% neq 0 exit 1
 cmake --build . --target install --config %CONFIG%
-if %errorlevel% neq 0 exit %errorlevel%
+if %errorlevel% neq 0 (
+    echo "If you see an error above about something not existing and mentioning the install target then you will need to add installing to your cmake script."
+    echo "the simplest way to do this is:"
+    echo "install (TARGETS YOURTARGET ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION lib)"
+    
+    exit 1
+)
 
+echo copy "%C_DLL%" "%PROJDIR%\embedded_files\%binfile%"
 copy "%C_DLL%" "%PROJDIR%\embedded_files\%binfile%"
-if %errorlevel% neq 0 exit %errorlevel%
+if %errorlevel% neq 0 exit 1
 
 if exist "%PROJDIR%\embedded_files\binaries.zip" del "%PROJDIR%\embedded_files\binaries.zip"
 
-echo powershell -ExecutionPolicy ByPass "%PROJDIR%\..\scripts\windows_zip.ps1" "%PROJDIR%\embedded_files"
-powershell -ExecutionPolicy ByPass "%PROJDIR%\..\scripts\windows_zip.ps1" "%PROJDIR%\embedded_files"
-if %errorlevel% neq 0 exit %errorlevel%
+echo powershell -ExecutionPolicy ByPass "%~dp0\windows_zip.ps1" "%PROJDIR%\embedded_files"
+powershell -ExecutionPolicy ByPass "%~dp0\windows_zip.ps1" "%PROJDIR%\embedded_files"
+if %errorlevel% neq 0 exit 1
 
 cd %CURRDIR%
 
+echo "------------NATIVE CODE BUILD COMPLETED SUCCESSFULLY"
 exit 0
