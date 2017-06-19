b2b4ea2b92208dd5ec32c446ef70663fac2a5c88
Tom Mason <wheybags@wheybags.com>
2017-06-19 10:14:32 +0100

    get rid of separate terminal on linux

--- scripts/linux_pre_build.sh
+++ scripts/linux_pre_build.sh
@@ -7,24 +7,6 @@ CONFIG=$2
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -z "$DISPLAY" ]; then
-    PRE_BUILD_NO_TERM=true
-fi
-
-termCommand="xterm -e"
-type mate-terminal >/dev/null 2>&1 && termCommand="mate-terminal --title=Building_native_code... --disable-factory -x bash -c"
-# no gnome-terminal support because they broke --disable-factory https://github.com/IgnorantGuru/spacefm/issues/428
-
-if [ -z "$PRE_BUILD_NO_TERM" ]; then
-    STATUSFILE=$(mktemp)
-    $termCommand "PRE_BUILD_NO_TERM=true /bin/bash \"$0\" \"$1\" \"$2\"; EXIT_STATUS=\$?; echo; echo EXIT STATUS: \$EXIT_STATUS; echo \$EXIT_STATUS > $STATUSFILE; read -n1 -r -p 'Press any key to continue...'"
-    if [ $? -ne 0 ]; then exit 1; fi
-    STATUS=$(cat $STATUSFILE)
-    rm $STATUSFILE
-    exit $STATUS
-fi
-
-
 if [ ! -e "`sed -n 2p "$PROJDIR/native_code_setting.txt"`" ]; then
     echo "#################################################"
     echo "Your native source code directory doesn't exist!"
