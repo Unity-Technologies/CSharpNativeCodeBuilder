1460564761798870ba5ade6cccc65f59fa07b624
Colm Vize <colm.vize@unity3d.com>
2021-09-09 18:30:22 +0100

    Improved NativeCodeBuilder, now uses XML instead of hard to use text file

--- .paket/get_paket.bat
+++
@@ -1,3 +0,0 @@
-SET mypath=%~dp0
-
-"%mypath%paket.bootstrapper.exe" "5.198.0"
---
+++ NativeCodeBuilder/Artomatix.NativeCodeBuilder/NativeBinaryExtractor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Artomatix.NativeCodeBuilder
+{
+    public enum Platform
+    {
+        Windows,
+        OSX,
+        Linux
+    }
+
+    internal class NativeBinaryExtractor
+    {
+        public static Platform GetCurrentPlatform()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    // Well, there are chances MacOSX is reported as Unix instead of MacOSX.
+                    // Instead of platform check, we'll do a feature checks (Mac specific root folders)
+                    if (Directory.Exists("/Applications")
+                        & Directory.Exists("/System")
+                        & Directory.Exists("/Users")
+                        & Directory.Exists("/Volumes"))
+                    {
+                        return Platform.OSX;
+                    }
+                    else
+                    {
+                        return Platform.Linux;
+                    }
+
+                case PlatformID.MacOSX:
+                    return Platform.OSX;
+
+                default:
+                    return Platform.Windows;
+            }
+        }
+
+        public static string GetArchString()
+        {
+            if (Environment.Is64BitProcess)
+                return "x64";
+            else
+                return "x86";
+        }
+    }
+}
